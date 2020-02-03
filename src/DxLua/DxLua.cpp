527489a170f6edc8b84872218501ce75bab4bcbf
れみろう <remyroez+github@gmail.com>
2020-02-03 23:23:40 +0900

    :sparkles: DxLua モジュールのインストール関数およびソースを追加

---
+++ src/DxLua/DxLua.cpp
@@ -0,0 +1,16 @@
+﻿#include <lua.hpp>
+#include <sol/sol.hpp>
+
+#include "DxLua.h"
+
+namespace DxLua {
+
+sol::table openDxLua(sol::this_state s)
+{
+    sol::state_view lua(s);
+    sol::table library = lua.create_table();
+
+    return library;
+}
+
+} // namespace DxLua
