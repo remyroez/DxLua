df595ff6258d7ef8d1f8dc2f434fc019eaafee8c
れみろう <remyroez+github@gmail.com>
2020-04-12 20:45:48 +0900

    :wrench: スクリプトのインデントをスペースに変換

--- examples/example3d-17.lua
+++ examples/example3d-17.lua
@@ -7,30 +7,30 @@ local AnimAttachIndex
 
 -- ＤＸライブラリの初期化
 function dx.Init()
-	-- ３Ｄモデルの読み込み
-	ModelHandle = dx.MV1LoadModel("DxChara.x")
+    -- ３Ｄモデルの読み込み
+    ModelHandle = dx.MV1LoadModel("DxChara.x")
 
-	-- 走りアニメーションをアタッチ
-	AnimAttachIndex = dx.MV1AttachAnim(ModelHandle, 1)
+    -- 走りアニメーションをアタッチ
+    AnimAttachIndex = dx.MV1AttachAnim(ModelHandle, 1)
 
-	-- 走りアニメーションの総時間を取得しておく
-	AnimTotalTime = dx.MV1GetAttachAnimTotalTime(ModelHandle, AnimAttachIndex)
+    -- 走りアニメーションの総時間を取得しておく
+    AnimTotalTime = dx.MV1GetAttachAnimTotalTime(ModelHandle, AnimAttachIndex)
 
-	-- アニメーション再生時間を初期化
-	AnimNowTime = 0.0
-	dx.MV1SetAttachAnimTime(ModelHandle, AnimAttachIndex, AnimNowTime)
+    -- アニメーション再生時間を初期化
+    AnimNowTime = 0.0
+    dx.MV1SetAttachAnimTime(ModelHandle, AnimAttachIndex, AnimNowTime)
 
-	-- 描画先を裏画面にする
-	dx.SetDrawScreen(dx.DX_SCREEN_BACK)
+    -- 描画先を裏画面にする
+    dx.SetDrawScreen(dx.DX_SCREEN_BACK)
 
-	-- カメラの位置と向きを設定
-	dx.SetCameraPositionAndTarget_UpVecY(dx.VGet(1340.0, 600.0, -420.0), dx.VGet(0.0, 400.0, -420.0))
+    -- カメラの位置と向きを設定
+    dx.SetCameraPositionAndTarget_UpVecY(dx.VGet(1340.0, 600.0, -420.0), dx.VGet(0.0, 400.0, -420.0))
 
-	-- カメラのクリッピング距離を設定
-	dx.SetCameraNearFar(16.0, 3800.0)
+    -- カメラのクリッピング距離を設定
+    dx.SetCameraNearFar(16.0, 3800.0)
 
-	-- 背景の色を灰色にする
-	dx.SetBackgroundColor(128, 128, 128)
+    -- 背景の色を灰色にする
+    dx.SetBackgroundColor(128, 128, 128)
 end
 
 -- メインループ(何かキーが押されたらループを抜ける)
