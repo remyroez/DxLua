bd7f0600b62975a3549270d0ebccb66b17403213
れみろう <remyroez+github@gmail.com>
2020-04-27 21:27:44 +0900

    :wrench: ログ出力機能関数を移植

--- src/DxLua/luaapi_log.cpp
+++ src/DxLua/luaapi_log.cpp
@@ -37,11 +37,16 @@ void port_log(sol::state_view &lua, sol::table &t) {
 #ifndef DX_NON_PRINTF_DX
 
 	// ログ出力機能関数
-	//extern	int			SetLogDrawOutFlag(int DrawFlag);									// printfDx の結果を画面に出力するかどうかを設定する、TRUE:出力を行う  FALSE:出力を行わない( printfDx を実行すると内部で SetLogDrawOutFlag( TRUE ) ; が呼ばれます )
-	//extern 	int			GetLogDrawFlag(void);											// printfDx の結果を画面に出力するかどうかの設定を取得する( 戻り値  TRUE:出力を行う  FALSE:出力を行わない )
-	//extern	int			SetLogFontSize(int Size);										// printfDx の結果を画面に出力する際に使用するフォントのサイズを設定する
-	//extern	int			SetLogFontHandle(int FontHandle);									// printfDx の結果を画面に出力する際に使用するフォントのハンドルを変更する
-	//extern	int			SetLogDrawArea(int x1, int y1, int x2, int y2);					// printfDx の結果を画面に出力する際の描画する領域を設定する
+	t["SetLogDrawOutFlag"] = [](sol::variadic_args va) {
+		int DrawFlag = va_get(va, 0, false);
+		return SetLogDrawOutFlag(DrawFlag);
+	};
+	t["GetLogDrawFlag"] = []() {
+		return GetLogDrawFlag() ? TRUE : FALSE;
+	};
+	DXLUA_PORT(t, SetLogFontSize);
+	DXLUA_PORT(t, SetLogFontHandle);
+	DXLUA_PORT(t, SetLogDrawArea);
 
 	// 簡易画面出力関数
 	//extern 	int			printfDx(const TCHAR * FormatString, ...);								// printf と同じ引数で画面に文字列を表示するための関数
